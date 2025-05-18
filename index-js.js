a2bc0899f0f3f5b4a02a08a87d3cf269a90303a0
Daniel Molina <danielmolina.dev@gmail.com>
2025-05-18 12:36:41 +0200

    feat: add send tx fn with simulate contract

--- index-js.js
+++ index-js.js
@@ -1,19 +1,74 @@
-import { createWalletClient, custom } from "https://esm.sh/viem";
+import {
+  createWalletClient,
+  custom,
+  createPublicClient,
+  defineChain,
+  parseEther,
+} from "https://esm.sh/viem";
+import { contractAddress, coffeeAbi } from "./constants-js.js";
 
 const connectButton = document.getElementById("connectButton");
+const fundButton = document.getElementById("fundButton");
+const ethAmountInput = document.getElementById("ethAmount");
 
 let walletClient;
+let publicClient;
 
 async function connect() {
   if (typeof window.ethereum !== "undefined") {
     walletClient = createWalletClient({
       transport: custom(window.ethereum),
     });
-    await walletClient.requestAddresses();
+    const [connectedAccount] = await walletClient.requestAddresses();
     connectButton.innerHTML = "Connected!";
+
+    return connectedAccount;
   } else {
     connectButton.innerHTML = "Please install MetaMask!";
   }
 }
 
+async function fund() {
+  const ethAmount = ethAmountInput.value;
+  console.log(`Funding with ${ethAmount} eth...`);
+  const connectedAccount = await connect();
+  const currentChain = await getCurrentChain(walletClient);
+
+  publicClient = createPublicClient({
+    transport: custom(window.ethereum),
+  });
+
+  const { request } = await publicClient.simulateContract({
+    address: contractAddress,
+    abi: coffeeAbi,
+    functionName: "fund",
+    account: connectedAccount,
+    chain: currentChain,
+    value: parseEther(ethAmount),
+  });
+
+  const hash = await walletClient.writeContract(request);
+  console.log("hash", hash);
+}
+
+async function getCurrentChain(client) {
+  const chainId = await client.getChainId();
+  const currentChain = defineChain({
+    id: chainId,
+    name: "Custom Chain",
+    nativeCurrency: {
+      name: "Ether",
+      symbol: "ETH",
+      decimals: 18,
+    },
+    rpcUrls: {
+      default: {
+        http: ["http://localhost:8545"],
+      },
+    },
+  });
+  return currentChain;
+}
+
 connectButton.onclick = connect;
+fundButton.onclick = fund;
